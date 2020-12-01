bd353ed740920ec3d636d6afc2fa8db5ab2d7805
HristoTsekov <xtsekooov@gmail.com>
2020-12-01 19:58:06 +0200

    Tab Adapter

---
+++ app/src/main/java/com/example/myapplication/ui/adapters/TabAdapter.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.ui.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.myapplication.ui.fragments.DailyQuoteFragment;
+import com.example.myapplication.ui.fragments.MyQuotesFragment;
+
+import javax.inject.Inject;
+
+
+public class TabAdapter extends FragmentStateAdapter {
+
+    private DailyQuoteFragment dailyQuoteFragment;
+    private MyQuotesFragment myQuotesFragment;
+
+    @Inject
+    public TabAdapter(FragmentManager fragmentManager, Lifecycle lifecycle, MyQuotesFragment myQuotesFragment, DailyQuoteFragment dailyQuoteFragment) {
+        super(fragmentManager, lifecycle);
+        this.dailyQuoteFragment = dailyQuoteFragment;
+        this.myQuotesFragment = myQuotesFragment;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return dailyQuoteFragment;
+            case 2:
+                return myQuotesFragment;
+            default:
+                throw new IllegalArgumentException("Unavailable tab");
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
