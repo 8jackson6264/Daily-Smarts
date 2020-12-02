7ba42792216aacdb2fdd62f751ec27af92ede763
nancy jackson8 <gnr.axlrose.6.2.1962@gmail.com>
2020-12-02 19:45:00 +0200

    for teo :D

---
+++ app/src/main/java/com/example/myapplication/data/Quote.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "quotes")
+public class Quote {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "quote")
+    public String quoteText;
+
+    @ColumnInfo(name = "author")
+    public String quoteAuthor;
+
+    public Quote() {
+    }
+
+    public Quote(String quoteText, String quoteAuthor) {
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/data/api/Api.java
@@ -0,0 +1,71 @@
+package com.example.myapplication.data.api;
+
+import com.example.myapplication.data.Quote;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Api {
+        private static Api instance;
+        private final ApiQuoteService service;
+
+        private Api() {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.forismatic.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            service = retrofit.create(ApiQuoteService.class);
+        }
+
+        public static Api getInstance() {
+            if (instance == null) instance = new Api();
+            return instance;
+        }
+
+        public void getRandomEngQuote(final ApiListener listener) {
+            service.getRandomEngQuote().enqueue(new Callback<Quote>() {
+                @Override
+                public void onResponse(Call<Quote> call, Response<Quote> response) {
+                    if (response.isSuccessful()) {
+                        listener.onQuoteReceived(response.body().getQuoteText(), response.body().getQuoteAuthor());
+                    } else {
+                        listener.onFailure();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Quote> call, Throwable t) {
+                    listener.onFailure();
+                }
+            });
+        }
+
+        public void getRandomRusQuote(final ApiListener listener) {
+            service.getRandomRusQuote().enqueue(new Callback<Quote>() {
+                @Override
+                public void onResponse(Call<Quote> call, Response<Quote> response) {
+                    if (response.isSuccessful()) {
+                        listener.onQuoteReceived(response.body().getQuoteText(), response.body().getQuoteAuthor());
+                    } else {
+                        listener.onFailure();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Quote> call, Throwable t) {
+                    listener.onFailure();
+                }
+            });
+        }
+
+        public interface ApiListener {
+            void onQuoteReceived(String quote, String author);
+
+            void onFailure();
+        }
+
+    }
