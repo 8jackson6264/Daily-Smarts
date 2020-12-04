75d0585ae06236a2f6c3f2474700c84162f720b1
nancy jackson8 <gnr.axlrose.6.2.1962@gmail.com>
2020-12-04 23:33:18 +0200

    on my way to build recycler view

---
+++ app/src/main/java/com/example/myapplication/ui/adapters/MyQuotesAdapter.java
@@ -0,0 +1,67 @@
+package com.example.myapplication.ui.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.data.Quote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyQuotesAdapter extends RecyclerView.Adapter<MyQuotesAdapter.ViewHolder>{
+
+    List<Quote> quotesList;
+
+    public MyQuotesAdapter() {
+        this.quotesList = new ArrayList<>();
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_quote,
+                parent,
+                false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtAuthor.setText(quotesList.get(position).quoteAuthor);
+        holder.txtQuote.setText(quotesList.get(position).quoteText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return quotesList.size();
+    }
+
+    public void setQuotesList(List<Quote> quotesList) {
+        this.quotesList = quotesList;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView btnSave;
+        ImageView btnShare;
+        TextView txtQuote;
+        TextView txtAuthor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            btnSave = itemView.findViewById(R.id.btn_save);
+            btnShare = itemView.findViewById(R.id.btn_share);
+            txtQuote = itemView.findViewById(R.id.txt_quote);
+            txtAuthor = itemView.findViewById(R.id.txt_author);
+
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/fragments/MyQuotesFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragments/MyQuotesFragment.java
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,19 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.data.Quote;
 import com.example.myapplication.data.api.Api;
+import com.example.myapplication.data.database.QuoteDatabaseService;
 import com.example.myapplication.databinding.FragmentMyQuotesBinding;
+import com.example.myapplication.ui.adapters.MyQuotesAdapter;
 
 import javax.inject.Inject;
 
 public class MyQuotesFragment extends BaseFragment <FragmentMyQuotesBinding>{
+
+    private MyQuotesAdapter myQuotesAdapter;
+    @Inject
+    QuoteDatabaseService quoteDatabaseService;
 
     @Inject
     public MyQuotesFragment() {
@@ -23,11 +31,16 @@ public class MyQuotesFragment extends BaseFragment <FragmentMyQuotesBinding>{
 
     @Override
     protected void onFragmentCreated(View view, Bundle savedInstance) {
-
+        binding.quoteRecycle.setLayoutManager(
+                new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        myQuotesAdapter = new MyQuotesAdapter();
+        binding.quoteRecycle.setAdapter(myQuotesAdapter);
+        quoteDatabaseService.getAllQuotes(data -> myQuotesAdapter.setQuotesList(data));
     }
 
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_my_quotes;
     }
+
 }
